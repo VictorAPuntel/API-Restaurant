65cd00a6f1d86cf752dbdda51681dcab26936e51
VictorAPuntel <victor.a.puntel@gmail.com>
2025-08-06 20:40:17 -0300

    Criando projeto de API, configurando o typescript, criando rotas, middleware, controller e tratamento de erros

---
+++ src/routes/productsRoutes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { ProductController } from "@/controllers/productsController";
+
+const productsRoutes = Router();
+const productsController = new ProductController();
+
+productsRoutes.get("/", productsController.index);
+
+export { productsRoutes };
