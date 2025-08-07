ac8061edc36191af9e6359a36861fce6093c888f
VictorAPuntel <victor.a.puntel@gmail.com>
2025-08-07 00:13:02 -0300

    Criando rotas e controller de orders

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import { productsRoutes } from "./productsRoutes";
 import { tablesRoutes } from "./tablesRoutes";
 import { tablesSessionsRoutes } from "./tablesSessionsRoutes";
+import { ordersRoutes } from "./ordersRoutes";
 
 const routes = Router();
 routes.use("/products", productsRoutes);
 routes.use("/tables", tablesRoutes);
 routes.use("/tables-sessions", tablesSessionsRoutes);
+routes.use("/orders", ordersRoutes);
 
 export { routes };
